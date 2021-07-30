75b12be55bec79a553270d1bf162e3be9fa8eab9
PiyushBedi <bedipiyush74@gmail.com>
2021-07-30 09:59:52 +0530

    End Of Section 8

---
+++ API/Interfaces/IUserRepository.cs
@@ -0,0 +1,35 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using API.DTOs;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Interfaces
+{
+    public interface IUserRepository1
+    {
+        Task<MemberDto> GetMemberAsync();
+        Task<IEnumerable<MemberDto>> GetMembersAsync();
+        Task<AppUser> GetUserByIdAsync(int id);
+        Task<AppUser> GetUserByUsernameAsync(string username);
+        Task<IEnumerable<AppUser>> GetUsersAsync();
+        Task<bool> SaveAllAsync();
+        void Update(AppUser user);
+    }
+
+    public interface IUserRepository
+    { 
+        void Update(AppUser user);
+
+        Task<bool> SaveAllAsync();
+
+        Task<IEnumerable<AppUser>> GetUsersAsync();
+
+        Task<AppUser> GetUserByIdAsync(int id);
+        Task<AppUser> GetUserByUsernameAsync(string username);
+
+        Task<IEnumerable<MemberDto>> GetMembersAsync();
+
+        Task<MemberDto> GetMemberAsync(string username);
+    }
+} 
